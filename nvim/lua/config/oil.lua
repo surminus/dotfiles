21af17ee858fe5e0c1727b756227e72d7be9fc8f
Laura Martin <laura.martin@ably.com>
2024-08-14 11:51:01 +0100

    Revert "oil: open in new tab by default"
    This reverts commit 6aca96f28b3f6a989a1d39595342ca82519a9368.

--- nvim/lua/config/oil.lua
+++ nvim/lua/config/oil.lua
@@ -3,10 +3,6 @@ require("oil").setup({
   view_options = {
     show_hidden = true,
   },
-
-  keymaps = {
-    ["<CR>"] = { "actions.select", opts = { tab = true } },
-  }
 })
 
 vim.keymap.set("n", "<leader>e", "<CMD>Oil --float .<CR>",  { desc = "Open Oil explorer view" })
