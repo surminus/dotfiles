fbe52dd4ec33b5a55d472706d0eb4eea9d03208b
Laura Martin <laura.martin@ably.com>
2024-09-09 10:21:26 +0100

    goto-preview update

--- nvim/lua/config/goto-preview.lua
+++ nvim/lua/config/goto-preview.lua
@@ -1,3 +1,7 @@
-require('goto-preview').setup {
-  default_mappings = true,
-}
+require("goto-preview").setup({
+	width = 120,
+	height = 25,
+	default_mappings = true,
+})
+
+vim.keymap.set("n", "<leader>d", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true })
