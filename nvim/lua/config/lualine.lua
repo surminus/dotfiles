15492afa1596bb0eabd8e3490d4b3873bbc85b7a
Laura Martin <laura@surminus.com>
2024-09-15 10:40:04 +0100

    More nvim updates

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,13 +1,14 @@
-require("lualine").setup {
-  options = {
-    theme = "moonfly"
-  },
-  sections = {
-    lualine_x = {'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'},
-  },
-  tabline = {
-    lualine_a = {'tabs'},
-  }
-}
+require("lualine").setup({
+	options = {
+		theme = "moonfly",
+	},
+	sections = {
+		lualine_x = { "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+	tabline = {
+		lualine_a = { "tabs" },
+		lualine_z = { "buffers" },
+	},
+})
--- nvim/lua/config/oil.lua
+++ nvim/lua/config/oil.lua
@@ -1,8 +1,18 @@
 require("oil").setup({
-  skip_confirm_for_simple_edits = true,
-  view_options = {
-    show_hidden = true,
-  },
+	skip_confirm_for_simple_edits = true,
+	view_options = { show_hidden = true },
+	watch_for_changes = true,
+
+	float = {
+		padding = 10,
+		max_width = 0,
+		max_height = 0,
+		border = "rounded",
+		win_options = {
+			winblend = 0,
+		},
+		preview_split = "right",
+	},
 })
 
-vim.keymap.set("n", "<leader>e", "<CMD>Oil --float .<CR>",  { desc = "Open Oil explorer view" })
+vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
