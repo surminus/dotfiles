f4cc518a4e4299140298eb56cf5ad4d2c5c5ac98
Laura Martin <laura.martin@ably.com>
2024-12-31 11:39:40 +0000

    remove vim-go config, change buffer delete shortcut

--- nvim/lua/config/custom.lua
+++ nvim/lua/config/custom.lua
@@ -8,17 +8,6 @@ vim.opt.scrolloff = 999
 vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_next)
 vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_prev)
 
--- vim-go
-vim.g.go_code_completion_enabled = 0
-vim.g.go_def_mapping_enabled = 0
-vim.g.go_diagnostics_level = 0
-vim.g.go_doc_keywordprg_enabled = 0
-vim.g.go_gopls_enabled = 0
-vim.g.go_highlight_fields = 1
-vim.g.go_highlight_function_calls = 1
-vim.g.go_highlight_operators = 1
-vim.g.go_highlight_types = 1
-
 -- Browse
 vim.api.nvim_create_user_command("Browse", function(opts)
 	vim.fn.system({ "xdg-open", opts.fargs[1] })
@@ -38,7 +27,7 @@ vim.keymap.set("n", "H", ":bprevious<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "L", ":bnext<CR>", { noremap = true, silent = true })
 
 -- Close buffer
-vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>d", ":bdelete<CR>", { noremap = true, silent = true })
 
 -- Move between windows
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", { noremap = true, silent = true })
