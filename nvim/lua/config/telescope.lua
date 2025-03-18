f983105435323d96c9db555655d1fa3881cc1273
Laura Martin <laura.martin@ably.com>
2025-03-18 14:09:55 +0000

    telescope updates

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,19 +1,20 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>w', builtin.grep_string, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>g", builtin.jumplist, {})
+vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>s", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>w", builtin.grep_string, {})
 
 local actions = require("telescope.actions")
-require('telescope').setup({
-  defaults = {
-    file_ignore_patterns = { "node_modules", ".git" },
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-j>"] = actions.move_selection_next,
-      }
-    }
-  },
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = { "node_modules", ".git" },
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+			},
+		},
+	},
 })
